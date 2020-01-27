3cdee7549f19beb55e8a9d611f9525cb4d32babe
ubuntu <ubuntu@ip-172-31-32-139.ec2.internal>
2020-01-27 19:06:42 +0000

    refactored code

--- src/main/java/com/learn/e02/streams/StreamOperationsExercise.java
+++ src/main/java/com/learn/e02/streams/StreamOperationsExercise.java
@@ -16,15 +16,15 @@ public class StreamOperationsExercise {
 	static {
 
 		playerList = new ArrayList<Player>();
-		playerList.add(new Player("Vijay", 215, 6561, 823, "Australia"));
-		playerList.add(new Player("Amrit", 121, 3700, 685, "India"));
-		playerList.add(new Player("Bala", 11, 1561, 376, "WestIndies"));
-		playerList.add(new Player("Entaz", 14, 2261, 190, "SriLanka"));
-		playerList.add(new Player("Gayatri", 221, 11561, 923, "Australia"));
-		playerList.add(new Player("Chaman", 125, 1001, 62, "SriLanka"));
-		playerList.add(new Player("Bhuvan", 215, 7001, 562, "India"));
-		playerList.add(new Player("Lakha", 25, 1601, 79, "WestIndies"));
-		playerList.add(new Player("Kachra", 25, 3001, 62, "India"));
+		playerList.add(new Player("Vijay", 215, 6561, 823, new Country(1,"India")));
+		playerList.add(new Player("Amrit", 121, 3700, 685, new Country(1,"India")));
+		playerList.add(new Player("Bala", 11, 1561, 376, new Country(4,"WestIndies")));
+		playerList.add(new Player("Entaz", 14, 2261, 190,  new Country(3,"SriLanka")));
+		playerList.add(new Player("Gayatri", 221, 11561, 923, new Country(2,"Australia")));
+		playerList.add(new Player("Chaman", 125, 1001, 62, new Country(3,"SriLanka")));
+		playerList.add(new Player("Bhuvan", 215, 7001, 562, new Country(1,"India")));
+		playerList.add(new Player("Lakha", 25, 1601, 79, new Country(4,"WestIndies")));
+		playerList.add(new Player("Kachra", 25, 3001, 62, new Country(1,"India")));
 
 	}
 
@@ -37,7 +37,7 @@ public class StreamOperationsExercise {
 	public static void displayPlayersForCountry(String country) {
 
 		List<String> countrySpecificPlayerList = playerList.stream()
-				.filter(player -> player.getCountry().equalsIgnoreCase(country) && player.getHighestScore()>100)
+				.filter(player -> player.getCountry().getCountryName().equalsIgnoreCase(country) && player.getHighestScore()>100)
 				.map(p -> p.getPlayerName())
 				.collect(Collectors.toList());
 
@@ -59,7 +59,7 @@ public class StreamOperationsExercise {
 
 	public static int getAverageRunsByCountry(String countryName) {
 
-		Stream<Player> playerStream =playerList.stream().filter(players -> players.getCountry().equalsIgnoreCase(countryName));
+		Stream<Player> playerStream =playerList.stream().filter(players -> players.getCountry().getCountryName().equalsIgnoreCase(countryName));
 		Double avgRunsInDouble = playerStream.collect(Collectors.averagingDouble((Player p) ->p.getRuns()));
 		int avgRunsInInt = avgRunsInDouble.intValue();
 		return avgRunsInInt;
@@ -69,7 +69,7 @@ public class StreamOperationsExercise {
 	public static List<String> getPlayerNamesSorted() {
 
 		List<String> playerNamesList = 	playerList.stream()
-				.sorted(Comparator.comparing((Player p) -> p.getCountry())
+				.sorted(Comparator.comparing((Player p) -> p.getCountry().getCountryName())
 						.thenComparing((Player p) -> p.getMatchesPlayed()).reversed())
 				.map(p -> p.getPlayerName())
 				.collect(Collectors.toList());
@@ -81,7 +81,7 @@ public class StreamOperationsExercise {
 
 		Map<String, String> playerMap =	playerList.stream()
 				.filter(p -> p.getMatchesPlayed()>200)
-				.collect(Collectors.toMap(p -> p.getPlayerName(), p -> p.getCountry()));
+				.collect(Collectors.toMap(p -> p.getPlayerName(), p -> p.getCountry().getCountryName()));
 		return playerMap;
 
 	}
@@ -96,14 +96,14 @@ public class StreamOperationsExercise {
 	public static void findPlayer(String name, String country) {
 		
 		Optional<Player> studOptional =	playerList.stream()
-				.filter(player -> player.getPlayerName().equalsIgnoreCase(name) && player.getCountry().equalsIgnoreCase(country)).findAny();
+				.filter(player -> player.getPlayerName().equalsIgnoreCase(name) && player.getCountry().getCountryName().equalsIgnoreCase(country)).findAny();
 		studOptional.ifPresent(System.out::println);
 	
 	}
 	
 	public static boolean checkHighScorerByCountry(String country) {
 		
-	boolean playerFound = playerList.stream().anyMatch(player -> player.getCountry().equalsIgnoreCase(country) && player.getRuns() > 10000);
+	boolean playerFound = playerList.stream().anyMatch(player -> player.getCountry().getCountryName().equalsIgnoreCase(country) && player.getRuns() > 10000);
 	return playerFound;
 	
 	}
